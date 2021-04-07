fc993604b922717370ef0af51b0f4179225b8180
prashant-coditas <41581256+prashant-coditas@users.noreply.github.com>
2021-04-07 22:39:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Header from "./Header";
+import Body from "./Body";
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
